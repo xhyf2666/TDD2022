7da38d3e3153987075e6df1184b0abf1967aa2a6
xyx266617 <62925356+xyx266617@users.noreply.github.com>
2022-04-19 08:47:14 +0800

    First unit test and url mapping,dummpy view

--- functional_tests.py
+++ functional_tests.py
@@ -19,24 +19,6 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-do',self.browser.title),"Browser title was :"+self.browser.title
         sele.fail('Finish the test!')
 
-        #she is invited to enter a tp-do item straight away
-        #is tying fly-fishing lures)
-
-        #when she hits enter,the page updates,and now the page lists
-        # '1':Buy peacock feathers' as an item in a to-do list
-
-        #There is still a text box inviting her to add another item.
-        #She enter "Use peacock feathers to make a fly" (Edith is very methodical)
-
-        #The page updates again,and now shows both items on her list
-
-        #Edith wonders whether the site will remember her list.Then shee sees
-        #that the site has generated a unique URL for her -- there is some
-        #explanatory text to that effect.
-
-        #She visits that URL - her to-do is still there.
-
-        #Satisfied,she goes back to sleep.
 
 if __name__=='__main__':
     unittest.main(warnings='ignore')
