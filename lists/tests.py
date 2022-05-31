fa64968a6c64f44a4ed6f8af541c8d599baa57d9
xyx266617 <62925356+xyx266617@users.noreply.github.com>
2022-05-31 09:25:15 +0800

    adjust model to different lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item,List
 
 # Create your tests here.
 class SmokeTest(TestCase):
@@ -12,23 +12,33 @@ class SmokeTest(TestCase):
         self.assertTemplateUsed(response,'home.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_savinf_and_retrieving_items(self):
+        list_=List()
+        list_.save()
+        
         first_item=Item()
         first_item.text='The first (ever) list item'
+        first_item.list=list_
         first_item.save()
 
         second_item=Item()
         second_item.text='Item the second'
+        second_item.list=list_
         second_item.save()
 
+        saved_list=List.objects.first()
+        self.assertEqual(saved_list,list_)
+        
         saved_items=Item.objects.all()
         self.assertEqual(saved_items.count(),2)
 
         first_saved_item=saved_items[0]
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(first_saved_item.list,list_)
         self.assertEqual(second_saved_item.text,'Item the second')
+        self.assertEqual(second_saved_item.list,list_)
 
 class HomePageTest(TestCase):
     pass
@@ -39,8 +49,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_=List.objects.create()
+        Item.objects.create(text='itemey 1',list=list_)
+        Item.objects.create(text='itemey 2',list=list_)
 
         response=self.client.get('/lists/the-only-list-in-the-world/')
 
